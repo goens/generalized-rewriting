dd24d24fdf12b4867ae3470d75c10cea5d05b965
Andrés Goens <andres@goens.org>
2024-09-22 19:26:34 +0200

    add example from TPIL

---
+++ GeneralizedRewriting/Examples/UnorderedPairs.lean
@@ -0,0 +1,136 @@
+-- from TPIL: https://leanprover.github.io/theorem_proving_in_lean4/axioms_and_computation.html
+
+private def eqv (p₁ p₂ : α × α) : Prop :=
+  (p₁.1 = p₂.1 ∧ p₁.2 = p₂.2) ∨ (p₁.1 = p₂.2 ∧ p₁.2 = p₂.1)
+
+infix:50 " ~ " => eqv
+
+private theorem eqv.refl (p : α × α) : p ~ p :=
+  Or.inl ⟨rfl, rfl⟩
+
+private theorem eqv.symm : ∀ {p₁ p₂ : α × α}, p₁ ~ p₂ → p₂ ~ p₁
+  | (a₁, a₂), (b₁, b₂), (Or.inl ⟨a₁b₁, a₂b₂⟩) =>
+    Or.inl (by simp_all)
+  | (a₁, a₂), (b₁, b₂), (Or.inr ⟨a₁b₂, a₂b₁⟩) =>
+    Or.inr (by simp_all)
+
+private theorem eqv.trans : ∀ {p₁ p₂ p₃ : α × α}, p₁ ~ p₂ → p₂ ~ p₃ → p₁ ~ p₃
+  | (a₁, a₂), (b₁, b₂), (c₁, c₂), Or.inl ⟨a₁b₁, a₂b₂⟩, Or.inl ⟨b₁c₁, b₂c₂⟩ =>
+    Or.inl (by simp_all)
+  | (a₁, a₂), (b₁, b₂), (c₁, c₂), Or.inl ⟨a₁b₁, a₂b₂⟩, Or.inr ⟨b₁c₂, b₂c₁⟩ =>
+    Or.inr (by simp_all)
+  | (a₁, a₂), (b₁, b₂), (c₁, c₂), Or.inr ⟨a₁b₂, a₂b₁⟩, Or.inl ⟨b₁c₁, b₂c₂⟩ =>
+    Or.inr (by simp_all)
+  | (a₁, a₂), (b₁, b₂), (c₁, c₂), Or.inr ⟨a₁b₂, a₂b₁⟩, Or.inr ⟨b₁c₂, b₂c₁⟩ =>
+    Or.inl (by simp_all)
+
+private theorem is_equivalence : Equivalence (@eqv α) :=
+  { refl := eqv.refl, symm := eqv.symm, trans := eqv.trans }
+
+instance [de : DecidableEq α] {a₁ a₂ b₁ b₂ : α} : Decidable (@eqv α (a₁,a₂) (b₁,b₂)) :=
+  instDecidableOr (dp := instDecidableAnd (dp := de _ _) (dq := de _ _))
+                  (dq := instDecidableAnd (dp := de _ _) (dq := de _ _))
+
+instance uprodSetoid (α : Type u) : Setoid (α × α) where
+  r     := eqv
+  iseqv := is_equivalence
+
+def UProd (α : Type u) : Type u :=
+  Quotient (uprodSetoid α)
+
+namespace UProd
+
+def mk {α : Type} (a₁ a₂ : α) : UProd α :=
+  Quotient.mk' (a₁, a₂)
+
+notation "{ " a₁ ", " a₂ " }" => mk a₁ a₂
+
+theorem mk_eq_mk (a₁ a₂ : α) : {a₁, a₂} = {a₂, a₁} :=
+  Quot.sound (Or.inr ⟨rfl, rfl⟩)
+
+private def eqv (p₁ p₂ : α × α) : Prop :=
+  (p₁.1 = p₂.1 ∧ p₁.2 = p₂.2) ∨ (p₁.1 = p₂.2 ∧ p₁.2 = p₂.1)
+
+private theorem eqv.refl (p : α × α) : p ~ p :=
+  Or.inl ⟨rfl, rfl⟩
+private theorem eqv.symm : ∀ {p₁ p₂ : α × α}, p₁ ~ p₂ → p₂ ~ p₁
+  | (a₁, a₂), (b₁, b₂), (Or.inl ⟨a₁b₁, a₂b₂⟩) =>
+    Or.inl (by simp_all)
+  | (a₁, a₂), (b₁, b₂), (Or.inr ⟨a₁b₂, a₂b₁⟩) =>
+    Or.inr (by simp_all)
+private theorem eqv.trans : ∀ {p₁ p₂ p₃ : α × α}, p₁ ~ p₂ → p₂ ~ p₃ → p₁ ~ p₃
+  | (a₁, a₂), (b₁, b₂), (c₁, c₂), Or.inl ⟨a₁b₁, a₂b₂⟩, Or.inl ⟨b₁c₁, b₂c₂⟩ =>
+    Or.inl (by simp_all)
+  | (a₁, a₂), (b₁, b₂), (c₁, c₂), Or.inl ⟨a₁b₁, a₂b₂⟩, Or.inr ⟨b₁c₂, b₂c₁⟩ =>
+    Or.inr (by simp_all)
+  | (a₁, a₂), (b₁, b₂), (c₁, c₂), Or.inr ⟨a₁b₂, a₂b₁⟩, Or.inl ⟨b₁c₁, b₂c₂⟩ =>
+    Or.inr (by simp_all)
+  | (a₁, a₂), (b₁, b₂), (c₁, c₂), Or.inr ⟨a₁b₂, a₂b₁⟩, Or.inr ⟨b₁c₂, b₂c₁⟩ =>
+    Or.inl (by simp_all)
+
+private theorem is_equivalence : Equivalence (@eqv α) :=
+  { refl := eqv.refl, symm := eqv.symm, trans := eqv.trans }
+
+instance uprodSetoid (α : Type u) : Setoid (α × α) where
+  r     := eqv
+  iseqv := is_equivalence
+
+private def mem_fn (a : α) : α × α → Prop
+  | (a₁, a₂) => a = a₁ ∨ a = a₂
+
+-- auxiliary lemma for proving mem_respects
+private theorem mem_swap {a : α} :
+      ∀ {p : α × α}, mem_fn a p = mem_fn a (⟨p.2, p.1⟩)
+  | (a₁, a₂) => by
+    apply propext
+    apply Iff.intro
+    . intro
+      | Or.inl h => exact Or.inr h
+      | Or.inr h => exact Or.inl h
+    . intro
+      | Or.inl h => exact Or.inr h
+      | Or.inr h => exact Or.inl h
+
+
+private theorem mem_respects
+      : {p₁ p₂ : α × α} → (a : α) → p₁ ~ p₂ → mem_fn a p₁ = mem_fn a p₂
+  | (a₁, a₂), (b₁, b₂), a, Or.inl ⟨a₁b₁, a₂b₂⟩ => by simp_all
+  | (a₁, a₂), (b₁, b₂), a, Or.inr ⟨a₁b₂, a₂b₁⟩ => by simp_all; rw [mem_swap]
+
+def mem (a : α) (u : UProd α) : Prop :=
+  Quot.liftOn u (fun p => mem_fn a p) (fun _ _ e => mem_respects a e)
+
+infix:50 (priority := high) " ∈ " => mem
+
+theorem mem_mk_left (a b : α) : a ∈ {a, b} :=
+  Or.inl rfl
+
+theorem mem_mk_right (a b : α) : b ∈ {a, b} :=
+  Or.inr rfl
+
+theorem mem_or_mem_of_mem_mk {a b c : α} : c ∈ {a, b} → c = a ∨ c = b :=
+  fun h => h
+
+
+end UProd
+
+theorem ex1 {x y z w : Nat} : {{x,y},{z,w}} = {{w,z},{x,y}} := by
+   have h := UProd.mk_eq_mk {x,y} {z,w}
+   rw [h, UProd.mk_eq_mk w z]
+
+--instance {α} : HasEquiv (α × α) := instHasEquivOfSetoid
+--theorem ex1_exact := Quotient.exact ex1
+#check Quotient.exact ex1
+#check fun a b => Quotient.exact (UProd.mk_eq_mk a b)
+
+/-
+theorem UProd.pair_mk_congr : a₁ ~ a₂ → b₁ ~ b₂ → (a₁,b₁) ~ (a₂,b₂) := by
+  sorry -- false!
+
+theorem ex_false : ((1,2),(3,4)) ~ ((1,2),(4,3)) := UProd.pair_mk_congr (Or.inl ⟨rfl, rfl⟩)
+  (Or.inr ⟨rfl, rfl⟩)
+
+theorem ex_false' : ¬ ((1,2),(3,4)) ~ ((1,2),(4,3)) := by decide
+
+example : False := ex_false' ex_false
+-/
